1b5d229668bd1a31a73584b7b88459ca2549dd1b
Schroedingersdoraemon <1181822466@qq.com>
2020-09-08 23:16:54 +0800

    -

--- blocks.h
+++ blocks.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/*Icon*/	/*Command*/		        /*Update Interval*/         /*Update Signal*/
 	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_cmus.sh",      1,  0},
 	{" 💿 ","/home/dylan/prog/dwmblocks/functions/dwm_resources.sh", 5,  0},
 	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_netspeed.sh",  0,  0},
 	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_alsa.sh",      0,  0},
 	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_battery.sh",   10, 0},
 	{"📆 ", "/home/dylan/prog/dwmblocks/functions/dwm_date.sh",      60, 0},
-	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_weather.sh",   0, 0},
+	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_weather.sh",   0,  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
+static char delim[] = " ";
 static unsigned int delimLen = 20;
