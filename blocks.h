560d2e165f013980a1d3374eb6dd8b39c72ce9c6
Schroedingersdoraemon <1181822466@qq.com>
2020-09-09 16:45:29 +0800

    to relative path

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/         /*Update Signal*/
-	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_cmus.sh",      1,  0},
-	{" 💿 ","/home/dylan/prog/dwmblocks/functions/dwm_resources.sh", 5,  0},
-	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_netspeed.sh",  0,  0},
-	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_alsa.sh",      0,  0},
-	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_battery.sh",   10, 0},
-	{"📆 ", "/home/dylan/prog/dwmblocks/functions/dwm_date.sh",      60, 0},
-	{"",    "/home/dylan/prog/dwmblocks/functions/dwm_weather.sh",   0,  0},
+	{"",    "~/prog/dwmblocks/functions/dwm_cmus.sh",      1,  0},
+	{" 💿 ","~/prog/dwmblocks/functions/dwm_resources.sh", 5,  0},
+	{"",    "~/prog/dwmblocks/functions/dwm_netspeed.sh",  0,  0},
+	{"",    "~/prog/dwmblocks/functions/dwm_alsa.sh",      0,  0},
+	{"",    "~/prog/dwmblocks/functions/dwm_battery.sh",   10, 0},
+	{"📆 ", "~/prog/dwmblocks/functions/dwm_date.sh",      1,  0},
+	{"",    "~/prog/dwmblocks/functions/dwm_weather.sh",   0,  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
