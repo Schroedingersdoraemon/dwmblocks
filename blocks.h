f303e5d35c7e34d0b6d97f581fdef18cacdf7fba
Schroedingersdoraemon <1181822466@qq.com>
2020-09-11 12:09:16 +0800

    added shortcut for update

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/         /*Update Signal*/
-	{"",    "~/prog/dwmblocks/functions/dwm_cmus.sh",      1,  0},
+	{"",    "~/prog/dwmblocks/functions/dwm_cmus.sh",      1,  1},
 	{" 💿 ","~/prog/dwmblocks/functions/dwm_resources.sh", 5,  0},
 	{"",    "~/prog/dwmblocks/functions/dwm_netspeed.sh",  0,  0},
-	{"",    "~/prog/dwmblocks/functions/dwm_alsa.sh",      0,  0},
+	{"",    "~/prog/dwmblocks/functions/dwm_alsa.sh",      0,  2},
 	{"",    "~/prog/dwmblocks/functions/dwm_battery.sh",   10, 0},
-	{"📆 ", "~/prog/dwmblocks/functions/dwm_date.sh",      1,  0},
+	{"📆 ", "~/prog/dwmblocks/functions/dwm_date.sh",      60, 0},
 	{"",    "~/prog/dwmblocks/functions/dwm_weather.sh",   0,  0},
 };
 
