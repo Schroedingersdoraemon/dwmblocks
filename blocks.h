c208b3ae261b44a2b1069ee69f73d623b0eedf51
Schroedingersdoraemon <1181822466@qq.com>
2020-11-07 19:19:07 +0800

    modified dwmblocks update command using kill/pkill

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 /*
     kill -34-(update signal) $(pidof dwmblocks)
-    pkill -RTMIN-(update signal) $(pidof dwmblocks)
+    pkill -RTMIN+(update signal) $(pidof dwmblocks)
 */
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/         /*Update Signal*/
