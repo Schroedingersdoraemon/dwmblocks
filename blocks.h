744094390dda200e270df9356decd50b8b6ce6dd
Schroedingersdoraemon <1181822466@qq.com>
2020-11-07 23:51:16 +0800

    cmus update interval 1 -> 5

--- blocks.h
+++ blocks.h
@@ -5,8 +5,8 @@
 */
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		        /*Update Interval*/         /*Update Signal*/
-	{"",    "~/prog/dwmblocks/functions/dwm_cmus.sh",      1,  1},
-	{" 💿","~/prog/dwmblocks/functions/dwm_resources.sh", 5,  0},
+	{"",    "~/prog/dwmblocks/functions/dwm_cmus.sh",      5,  1},
+	{" 💿","~/prog/dwmblocks/functions/dwm_resources.sh",  5,  0},
 	{"",    "~/prog/dwmblocks/functions/dwm_netspeed.sh",  0,  0},
 	{"",    "~/prog/dwmblocks/functions/dwm_alsa.sh",      0,  2},
 	{"",    "~/prog/dwmblocks/functions/dwm_battery.sh",   10, 0},
