cb20b666e35c73b5ba1231a0841482dd24c80a9a
Schroedingersdoraemon <1181822466@qq.com>
2021-06-09 15:21:19 +0800

    added dwm_mpd.sh to music script

--- functions/dwm_music.sh
+++ functions/dwm_music.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [ $(pgrep -x cmus | wc -l) -eq 0 ]; then
+if [ $(pgrep -x mpd | wc -l) -eq 1 ] && [ $(mpc|head -1|wc -l) -eq 1 ]; then
+	bash $HOME/prog/dwmblocks/functions/dwm_mpd.sh
+elif [ $(pgrep -x netease-cloud| wc -l) -lt 1 ]; then
 	bash $HOME/prog/dwmblocks/functions/dwm_netease.sh
-else
+elif [ $(pgrep -x cmus| wc -l) -eq 1]; then
 	bash $HOME/prog/dwmblocks/functions/dwm_cmus.sh
 fi
