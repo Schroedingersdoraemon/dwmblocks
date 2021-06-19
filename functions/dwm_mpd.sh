d2f492bee3e6e7edd8f587d6220c6fbcc40109ee
Schroedingersdoraemon <1181822466@qq.com>
2021-06-19 20:21:21 +0800

    play/pause icon for mpd

--- functions/dwm_mpd.sh
+++ functions/dwm_mpd.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 name=$(mpc | head -1)
 status=$(mpc status | sed -n '2p' | awk '{print $1}')
+shuffle=$(mpc status | grep 'random: on' | wc -l)
+case $shuffle in
+	0)
+		shuffle_status='' ;;
+	1)
+		shuffle_status='🔀' ;;
+esac
+
 case $status in
 	\[playing\])
-		printf "%s %s" "▶" "$name" ;;
+		printf "%s %s %s" "▶" "$name" "$shuffle_status";;
 	\[paused\])
-		printf "%s %s" "⏸" "$name" ;;
+		printf "%s %s %s" "⏸" "$name" "$shuffle_status";;
 esac
