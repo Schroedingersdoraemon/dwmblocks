3b34e492f823af9167959452c3e145bedda2abc1
Schroedingersdoraemon <1181822466@qq.com>
2021-06-19 20:20:52 +0800

    dwm music sequence

--- functions/dwm_music.sh
+++ functions/dwm_music.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ $(pgrep -x mpd | wc -l) -eq 1 ] && [ $(mpc|head -1|wc -l) -eq 1 ]; then
 	bash $HOME/prog/dwmblocks/functions/dwm_mpd.sh
+elif [ $(pgrep -x cmus| wc -l) -eq 1 ]; then
+	bash $HOME/prog/dwmblocks/functions/dwm_cmus.sh
 elif [ $(pgrep -x netease-cloud| wc -l) -lt 1 ]; then
 	bash $HOME/prog/dwmblocks/functions/dwm_netease.sh
-elif [ $(pgrep -x cmus| wc -l) -eq 1]; then
-	bash $HOME/prog/dwmblocks/functions/dwm_cmus.sh
 fi
